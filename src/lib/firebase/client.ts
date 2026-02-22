af38d36dba78bac47caf27d7229cfd2771010ba1
Revoltlution <bryan.fletcher@gmail.com>
2026-02-21 18:23:26 -0800

    getting ready to test pics and thumbnails in group

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -1,11 +1,14 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage"; // <--- ADD THIS IMPORT
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  // Storage needs this line to function:
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!, 
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
@@ -13,3 +16,4 @@ const firebaseConfig = {
 export const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
